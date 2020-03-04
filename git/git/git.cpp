16ae04e58bb3295c79fb33430b6dc790ea7cd7f5
ntutTutorial <asd30135931@gmail.com>
2020-03-04 23:56:38 +0800

    test

--- git/git/git.cpp
+++ git/git/git.cpp
@@ -1,26 +1,12 @@
-#include "stdafx.h"
 #include <string>
 #include <iostream>
 using namespace System;
-bool Guess(int number) {
-	static int target = -1;
-	if (target == -1) {
-		Random r;
-		target = r.Next() % 100 + 1;
-	}
-	if (number == target) {
-		std::cout << "Correct !!";
-		target = -1;
-		return true;
-	}
-	else std::cout << "Wrong" << std::endl;
-	return false;
-}
+
 int main(array<System::String ^> ^args)
 {
-	int guess;
-	do {
-		std::cin >> guess;
-	} while (!Guess(guess));
+	int a = 9;
+	int b = 0;
+	if (a == 9)
+		b += 1;
 	return 0;
 }
